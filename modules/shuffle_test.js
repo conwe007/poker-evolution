56d01f65ba881d8e1565fc1ae0a1e014c9ac49bb
conwe007 <conwelljack97@gmail.com>
2023-05-02 15:56:28 -0400

    commit

--- modules/shuffle_test.js
+++ modules/shuffle_test.js
@@ -3,6 +3,17 @@ import Entity from './entity.js';
 
 const NUM_RANKS = 9;
 const NUM_TRIALS = 10000;
+const HAND_RANK_HIGH_CARD = 0;
+const HAND_RANK_PAIR = 1;
+const HAND_RANK_TWO_PAIR = 2;
+const HAND_RANK_THREE_OF_A_KIND = 3;
+const HAND_RANK_STRAIGHT = 4;
+const HAND_RANK_FLUSH = 5;
+const HAND_RANK_FULL_HOUSE = 6;
+const HAND_RANK_FOUR_OF_A_KIND = 7;
+const HAND_RANK_STRAIGHT_FLUSH = 8;
+const LUT_WEIGHTS = ['c', 'd', 's', 'h', 'A', '2', '3', '4', '5', '6', '7', '8', '9', 'T', 'J', 'Q', 'K'];
+const LUT_HAND_RANK = ['high card', 'pair', 'two pair', 'three of a kind', 'straight', 'flush', 'full house', 'four of a kind', 'straight flush'];
 
 export default class ShuffleTest
 {
@@ -47,7 +58,9 @@ export default class ShuffleTest
     {
         let output = '';
 
+        output += LUT_WEIGHTS.toString();
         output += this.entity.weights.toString() + '\n';
+        output += LUT_HAND_RANK.toString();
         output += this.ranks.toString();
 
         return output;
