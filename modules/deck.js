27401f7b9a0566a7f83611f4615afe0f3f2f7775
conwe007 <conwelljack97@gmail.com>
2023-05-05 14:02:49 -0400

    commit

--- modules/deck.js
+++ modules/deck.js
@@ -35,7 +35,7 @@ export default class Deck
         {
             for(let index_secondary = index_primary + 1; index_secondary < NUM_CARDS_DECK; index_secondary++)
             {
-                if(this.cards[index_primary].weight > this.cards[index_secondary].weight)
+                if(this.cards[index_primary].weight < this.cards[index_secondary].weight)
                 {
                     let card_temporary = this.cards[index_primary];
                     this.cards[index_primary] = this.cards[index_secondary];
